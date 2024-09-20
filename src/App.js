200f8735f2a37aec7b1ea826ce7df13fd58e7950
arshin-khan1 <arshin.khan@accenture.com>
2024-09-20 12:48:40 +0530

    crud  code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router , Routes, Route, Link } from "react-router-dom";
+
+import EditComponent from './Components/Products/EditComponent';
+import ListComponent from './Components/Products/ListComponent';
+import CreateComponent from './Components/Products/CreateComponent'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Router>
+    <Navbar bg="primary">
+      <Container>
+        <Link to={"/"} className="navbar-brand text-white">
+          Basic Crud Appp
+        </Link>
+      </Container>
+    </Navbar>
+
+    <Container className="mt-5">
+      <Row>
+        <Col md={12}>
+        <Routes>
+            <Route path="/product/create" element={<CreateComponent />} />
+            <Route path="/product/edit/:id" element={<EditComponent />} />
+            <Route exact path='/' element={<ListComponent />} />
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
+  </Router>);
 }
 
 export default App;
